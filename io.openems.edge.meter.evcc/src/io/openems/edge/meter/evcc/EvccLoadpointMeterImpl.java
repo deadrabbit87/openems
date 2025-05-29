cf93e92572931cbea5b5c64d42c318b5829d9958
deadrabbit87 <maximilian.hauser@z8v.de>
2025-05-29 19:42:42 +0200

    Update EvccLoadpointMeterImpl.java

--- io.openems.edge.meter.evcc/src/io/openems/edge/meter/evcc/EvccLoadpointMeterImpl.java
+++ io.openems.edge.meter.evcc/src/io/openems/edge/meter/evcc/EvccLoadpointMeterImpl.java
@@ -207,8 +207,6 @@ public class EvccLoadpointMeterImpl extends AbstractOpenemsComponent
 			this.calculateProductionEnergy.update(-activePower);
 			this.calculateConsumptionEnergy.update(0);
 		}
-		System.out.println(this.getActivePowerL1());
-		System.out.println("Zählertyp " + getMeterType()); 
 	}
 
 	@Override
